3a2e4b49e31eb2acbcc436fb6f976cf89d93313c
Yevhen Khreptun <khreptun.e@gmail.com>
2023-07-13 16:37:58 -0400

    Updated basic styles. Created the Header component with styles. Add several components.

--- survey-app/frontend/src/App.jsx
+++ survey-app/frontend/src/App.jsx
@@ -1,5 +1,26 @@
+import React from "react";
+import { Route, Routes, Switch } from "react-router-dom";
+
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import Home from "./components/Home";
+import ContactUs from "./components/ContactUs";
+import NotFound from "./components/NotFound";
+
 const App = () => {
-  return <>Hello Polls Planet </>;
+  return (
+    <div className="website">
+      <Header />
+      <main>
+        <Routes>
+          <Route exact path="/" element={<Home />} />
+          <Route path="/contactus" element={<ContactUs />} />
+          <Route path="*" element={<NotFound />} />
+        </Routes>
+      </main>
+      <Footer />
+    </div>
+  );
 };
 
 export default App;
