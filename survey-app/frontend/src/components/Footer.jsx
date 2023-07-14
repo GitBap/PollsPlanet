ecacc7934983d2160f0a3b9337890548745f2670
Yevhen Khreptun <khreptun.e@gmail.com>
2023-07-13 21:02:03 -0400

    Added footer

--- survey-app/frontend/src/components/Footer.jsx
+++ survey-app/frontend/src/components/Footer.jsx
@@ -1,7 +1,27 @@
 import React from "react";
+import { Link } from "react-router-dom";
+
+import "./styles/footer.scss";
 
 const Footer = () => {
-  return <footer>Footer</footer>;
+  return (
+    <footer>
+      <div className="container">
+        <div className="wrapper">
+          <p>Copyright © 2023</p>
+          <p>
+            <span>View </span>
+            <Link
+              target="_blank"
+              to={"https://github.com/PaoloJr90/PollsPlanet"}
+            >
+              code
+            </Link>
+          </p>
+        </div>
+      </div>
+    </footer>
+  );
 };
 
 export default Footer;
