12516b4e59cf938cd2746d53e978e31d5994205e
PJ Angeloni <pj.angeloni@gmail.com>
2023-07-15 10:34:04 -0400

    updated database script

--- survey-app/backend/database.sql
+++ survey-app/backend/database.sql
@@ -18,6 +18,7 @@ CREATE TABLE QUESTIONS (
     id SERIAL PRIMARY KEY,
     -- tenant_id VARCHAR(255) NOT NULL UNIQUE,
     survey_id INT NOT NULL,
+    question_id INT NOT NULL,
     question TEXT NOT NULL,
     -- type VARCHAR(255) NOT NULL, -- boolean, multiple_choice, text
     -- language VARCHAR(10) NOT NULL, -- english, french
@@ -46,11 +47,11 @@ CREATE TABLE ANSWERS (
     id SERIAL PRIMARY KEY,
     -- tenant_id VARCHAR(255) NOT NULL UNIQUE, -- organization name
     survey_id INT NOT NULL,
-    -- question_id INT NOT NULL,
+    question_id INT NOT NULL,
     answer VARCHAR(255) NOT NULL,
     created_at TIMESTAMP NOT NULL DEFAULT NOW(),
     FOREIGN KEY (survey_id) REFERENCES SURVEYS(id)
-    -- FOREIGN KEY (question_id) REFERENCES QUESTIONS(id)
+    FOREIGN KEY (question_id) REFERENCES QUESTIONS(question_id)
 );
 
 -- CREATE TABLE USERS (
@@ -85,23 +86,23 @@ CREATE TABLE ANSWERS (
 
 INSERT INTO surveys (title)
 VALUES ('First Survey');
-INSERT INTO questions (survey_id, question)
-VALUES (1, 'On a scale of 1-10, what is your level happiness?');
-INSERT INTO questions (survey_id, question)
-VALUES (1, 'On a scale of 1-10, how hungry are you?' );
-INSERT INTO questions (survey_id, question)
-VALUES (1, 'On a scale of 1-10,how rich are you?');
-INSERT INTO questions (survey_id, question)
-VALUES (1, 'On a scale of 1-10, how much do you like surveys?');
-INSERT INTO questions (survey_id, question)
-VALUES (1, 'On a scale of 1-10, how much do you know programming?');
-INSERT INTO questions (survey_id, question)
-VALUES (1,'On a scale of 1-10, how much do you like studying?' );
-INSERT INTO questions (survey_id, question)
-VALUES (1, 'On a scale of 1-10, how likely would recommend this survey to your friend?');
-INSERT INTO questions (survey_id, question)
-VALUES (1, 'On a scale of 1-10, how thirsty are you?');
-INSERT INTO questions (survey_id, question)
-VALUES (1, 'On a scale of 1-10, how willing are you learn about programming?');
-INSERT INTO questions (survey_id, question)
-VALUES (1, 'On a scale of 1-10, how much do you like to eat?');
+INSERT INTO questions (survey_id, question_id, question)
+VALUES (1, 1, 'On a scale of 1-10, what is your level happiness?');
+INSERT INTO questions (survey_id, question_id, question)
+VALUES (1, 2, 'On a scale of 1-10, how hungry are you?' );
+INSERT INTO questions (survey_id, question_id, question)
+VALUES (1, 3,  'On a scale of 1-10,how rich are you?');
+INSERT INTO questions (survey_id, question_id, question)
+VALUES (1, 4, 'On a scale of 1-10, how much do you like surveys?');
+INSERT INTO questions (survey_id, question_id, question)
+VALUES (1, 5, 'On a scale of 1-10, how much do you know programming?');
+INSERT INTO questions (survey_id, question_id, question)
+VALUES (1, 6,'On a scale of 1-10, how much do you like studying?' );
+INSERT INTO questions (survey_id, question_id, question)
+VALUES (1, 7, 'On a scale of 1-10, how likely would recommend this survey to your friend?');
+INSERT INTO questions (survey_id, question_id, question)
+VALUES (1, 8, 'On a scale of 1-10, how thirsty are you?');
+INSERT INTO questions (survey_id, question_id, question)
+VALUES (1, 9, 'On a scale of 1-10, how willing are you learn about programming?');
+INSERT INTO questions (survey_id, question_id, question)
+VALUES (1, 10, 'On a scale of 1-10, how much do you like to eat?');
