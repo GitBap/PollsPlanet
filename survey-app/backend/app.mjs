5c085c43499c7296231e8d88850d556a01febc5c
Yevhen Khreptun <khreptun.e@gmail.com>
2023-07-15 10:43:07 -0400

    Middleware for cors policy with Access-Control-Allow-Origin

--- survey-app/backend/app.mjs
+++ survey-app/backend/app.mjs
@@ -8,6 +8,12 @@ const app = express();
 app.use(express.json());
 app.use(express.static("public"));
 
+// Create a proxy server to avoid the issues with access to fetch hasn't been blocked by cors policy
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  next();
+});
+
 //router entrance for administrators function
 app.use("/api/users", usersAdmin);
 //router entrance for polls
