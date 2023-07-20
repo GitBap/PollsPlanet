8cb660fe419ad09ea1f37ca99b78f0da93172ce8
Ke Qi <shellaqi010@gmail.com>
2023-07-20 11:11:56 -0400

    add user_id FOREIGN KEY in survey table

--- survey-app/backend/database.sql
+++ survey-app/backend/database.sql
@@ -7,16 +7,20 @@ CREATE DATABASE polls_planet;
 -- to use uuid in various tables
 CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
 
+DROP TABLE IF EXISTS SURVEYS;
 CREATE TABLE SURVEYS (
     id SERIAL PRIMARY KEY,
     -- tenant_id VARCHAR(255) NOT NULL UNIQUE, -- organization name
+    user_id INT,
     title VARCHAR(255) NOT NULL,
     -- description TEXT NOT NULL,
     -- category VARCHAR(255) NOT NULL, -- programming, education, etc
     -- language VARCHAR(10) NOT NULL, -- english, french
-    created_at TIMESTAMP NOT NULL DEFAULT NOW()
+    created_at TIMESTAMP NOT NULL DEFAULT NOW(),
+    FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE CASCADE
 );
 
+DROP TABLE IF EXISTS QUESTIONS;
 CREATE TABLE QUESTIONS (
     id SERIAL PRIMARY KEY,
     -- tenant_id VARCHAR(255) NOT NULL UNIQUE,
@@ -26,7 +30,7 @@ CREATE TABLE QUESTIONS (
     -- type VARCHAR(255) NOT NULL, -- boolean, multiple_choice, text
     -- language VARCHAR(10) NOT NULL, -- english, french
     created_at TIMESTAMP NOT NULL DEFAULT NOW(),
-    FOREIGN KEY (survey_id) REFERENCES SURVEYS(id)
+    FOREIGN KEY (survey_id) REFERENCES SURVEYS(id) ON DELETE CASCADE
 );
 
 -- CREATE TABLE USERS_SURVEYS_LINK (
@@ -46,6 +50,7 @@ CREATE TABLE QUESTIONS (
 --     FOREIGN KEY (question_id) REFERENCES QUESTIONS(id)
 -- );
 
+DROP TABLE IF EXISTS ANSWERS;
 CREATE TABLE ANSWERS (
     id SERIAL PRIMARY KEY,
     -- tenant_id VARCHAR(255) NOT NULL UNIQUE, -- organization name
@@ -53,10 +58,11 @@ CREATE TABLE ANSWERS (
     question_id INT NOT NULL,
     answer VARCHAR(255) NOT NULL,
     created_at TIMESTAMP NOT NULL DEFAULT NOW(),
-    FOREIGN KEY (survey_id) REFERENCES SURVEYS(id),
-    FOREIGN KEY (question_id) REFERENCES QUESTIONS(question_id)
+    FOREIGN KEY (survey_id) REFERENCES SURVEYS(id) ON DELETE CASCADE,
+    FOREIGN KEY (question_id) REFERENCES QUESTIONS(id) ON DELETE CASCADE
 );
 
+DROP TABLE IF EXISTS USERS;
 CREATE TABLE USERS (
     id SERIAL PRIMARY KEY,
     -- tenant_id VARCHAR(255) NOT NULL UNIQUE, -- organization name
@@ -80,7 +86,8 @@ CREATE TABLE USERS (
 );
 
 --     user sessions; something to work on later
-    CREATE TABLE USER_SESSIONS (
+DROP TABLE IF EXISTS USER_SESSIONS;
+CREATE TABLE USER_SESSIONS (
     id SERIAL PRIMARY KEY,
     email VARCHAR(255) REFERENCES USERS(email),
     session_id VARCHAR(255) NOT NULL,
@@ -90,54 +97,54 @@ CREATE TABLE USERS (
 );
 
 -- INSERT for surveys
-INSERT INTO surveys (title)
-VALUES ('First Survey');
-INSERT INTO surveys (title)
-VALUES ('Second Survey');
-INSERT INTO surveys (title)
-VALUES ('Third Survey');
+INSERT INTO surveys (user_id,title)
+VALUES (2,'First Survey');
+INSERT INTO surveys (user_id,title)
+VALUES (2,'Second Survey');
+INSERT INTO surveys (user_id,title)
+VALUES (2,'Third Survey');
 
 -- INSERT for questions
-INSERT INTO questions (survey_id, question_id, question)
-VALUES (1, 1, 'On a scale of 1-10, what is your level happiness?');
-INSERT INTO questions (survey_id, question_id, question)
-VALUES (1, 2, 'On a scale of 1-10, how hungry are you?' );
-INSERT INTO questions (survey_id, question_id, question)
-VALUES (1, 3,  'On a scale of 1-10,how rich are you?');
-INSERT INTO questions (survey_id, question_id, question)
-VALUES (1, 4, 'On a scale of 1-10, how much do you like surveys?');
-INSERT INTO questions (survey_id, question_id, question)
-VALUES (1, 5, 'On a scale of 1-10, how much do you know programming?');
-INSERT INTO questions (survey_id, question_id, question)
-VALUES (1, 6,'On a scale of 1-10, how much do you like studying?' );
-INSERT INTO questions (survey_id, question_id, question)
-VALUES (1, 7, 'On a scale of 1-10, how likely would recommend this survey to your friend?');
-INSERT INTO questions (survey_id, question_id, question)
-VALUES (1, 8, 'On a scale of 1-10, how thirsty are you?');
-INSERT INTO questions (survey_id, question_id, question)
-VALUES (1, 9, 'On a scale of 1-10, how willing are you learn about programming?');
-INSERT INTO questions (survey_id, question_id, question)
-VALUES (1, 10, 'On a scale of 1-10, how much do you like to eat?');
-INSERT INTO questions (survey_id, question_id, question)
-VALUES (2, 11, 'what is your level happiness?');
-INSERT INTO questions (survey_id, question_id, question)
-VALUES (2, 12, 'how hungry are you?' );
-INSERT INTO questions (survey_id, question_id, question)
-VALUES (2, 13, 'how rich are you?');
-INSERT INTO questions (survey_id, question_id, question)
-VALUES (2, 14, 'how much do you like surveys?');
-INSERT INTO questions (survey_id, question_id, question)
-VALUES (2, 15, 'how much do you know programming?');
-INSERT INTO questions (survey_id, question_id, question)
-VALUES (2, 16,'how much do you like studying?' );
-INSERT INTO questions (survey_id, question_id, question)
-VALUES (2, 17, 'how likely would recommend this survey to your friend?');
-INSERT INTO questions (survey_id, question_id, question)
-VALUES (2, 18, 'how thirsty are you?');
-INSERT INTO questions (survey_id, question_id, question)
-VALUES (2, 19, 'how willing are you learn about programming?');
-INSERT INTO questions (survey_id, question_id, question)
-VALUES (2, 20, 'how much do you like to eat?');
+-- INSERT INTO questions (survey_id, question_id, question)
+-- VALUES (1, 1, 'On a scale of 1-10, what is your level happiness?');
+-- INSERT INTO questions (survey_id, question_id, question)
+-- VALUES (1, 2, 'On a scale of 1-10, how hungry are you?' );
+-- INSERT INTO questions (survey_id, question_id, question)
+-- VALUES (1, 3,  'On a scale of 1-10,how rich are you?');
+-- INSERT INTO questions (survey_id, question_id, question)
+-- VALUES (1, 4, 'On a scale of 1-10, how much do you like surveys?');
+-- INSERT INTO questions (survey_id, question_id, question)
+-- VALUES (1, 5, 'On a scale of 1-10, how much do you know programming?');
+-- INSERT INTO questions (survey_id, question_id, question)
+-- VALUES (1, 6,'On a scale of 1-10, how much do you like studying?' );
+-- INSERT INTO questions (survey_id, question_id, question)
+-- VALUES (1, 7, 'On a scale of 1-10, how likely would recommend this survey to your friend?');
+-- INSERT INTO questions (survey_id, question_id, question)
+-- VALUES (1, 8, 'On a scale of 1-10, how thirsty are you?');
+-- INSERT INTO questions (survey_id, question_id, question)
+-- VALUES (1, 9, 'On a scale of 1-10, how willing are you learn about programming?');
+-- INSERT INTO questions (survey_id, question_id, question)
+-- VALUES (1, 10, 'On a scale of 1-10, how much do you like to eat?');
+-- INSERT INTO questions (survey_id, question_id, question)
+-- VALUES (2, 11, 'what is your level happiness?');
+-- INSERT INTO questions (survey_id, question_id, question)
+-- VALUES (2, 12, 'how hungry are you?' );
+-- INSERT INTO questions (survey_id, question_id, question)
+-- VALUES (2, 13, 'how rich are you?');
+-- INSERT INTO questions (survey_id, question_id, question)
+-- VALUES (2, 14, 'how much do you like surveys?');
+-- INSERT INTO questions (survey_id, question_id, question)
+-- VALUES (2, 15, 'how much do you know programming?');
+-- INSERT INTO questions (survey_id, question_id, question)
+-- VALUES (2, 16,'how much do you like studying?' );
+-- INSERT INTO questions (survey_id, question_id, question)
+-- VALUES (2, 17, 'how likely would recommend this survey to your friend?');
+-- INSERT INTO questions (survey_id, question_id, question)
+-- VALUES (2, 18, 'how thirsty are you?');
+-- INSERT INTO questions (survey_id, question_id, question)
+-- VALUES (2, 19, 'how willing are you learn about programming?');
+-- INSERT INTO questions (survey_id, question_id, question)
+-- VALUES (2, 20, 'how much do you like to eat?');
 
 -- INSERT for users
 INSERT INTO users (name, email, password)
