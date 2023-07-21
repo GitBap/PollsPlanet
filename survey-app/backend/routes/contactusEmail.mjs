42b6490dbe44ca8e703b849733f35832c8c2c146
gitBap <109323238+GitBap@users.noreply.github.com>
2023-07-21 12:46:47 -0400

    email created

--- survey-app/backend/routes/contactusEmail.mjs
+++ survey-app/backend/routes/contactusEmail.mjs
@@ -1,37 +1,44 @@
-import express from 'express';
-import sendpulse from 'sendpulse-api';
+import express from "express";
+import sendpulse from "sendpulse-api";
 
-const API_USER_ID = 'c0d97dfd87b161f5364d3dc7bc263770';
-const API_SECRET = '82a894199bbde2dac2319e104b963f85';
+const API_USER_ID = "c0d97dfd87b161f5364d3dc7bc263770";
+const API_SECRET = "82a894199bbde2dac2319e104b963f85";
 const TOKEN_STORAGE = "/tmp/";
 
 const emailRouter = express.Router();
 
-emailRouter.post('/send', async (req, res) => {
-    const { name, email, message } = req.body;
+emailRouter.post("/send", async (req, res) => {
+  const { name, email, message } = req.body;
 
-    sendpulse.init(API_USER_ID, API_SECRET, TOKEN_STORAGE, (token) => {
-        if (token) {
-            console.log('Successfully authorized in SendPulse');
-            const from = {
-                name: 'Your Company Name',
-                email: 'yourcompany@example.com',  // we need to create an email for it 
-            };
-            const to = [{
-                email: 'someone@example.com',    // we need to create an email for it
-            }];
-            const subject = `Contact from ${name} (${email})`;
-            const body = message;
-            sendpulse.smtpSendMail((response) => {
-                console.log(response);
-                res.send(response);
-            }, from, to, subject, body);
-        } else {
-            console.error('Failed to authorize in SendPulse');
-            res.status(500).send('Failed to send email');
-        }
-    });
+  sendpulse.init(API_USER_ID, API_SECRET, TOKEN_STORAGE, (token) => {
+    if (token) {
+      console.log("Successfully authorized in SendPulse");
+      const from = {
+        name: "Your Company Name",
+        email: "pollsplanet1@gmail.com",
+      };
+      const to = [
+        {
+          email: "pollsplanet1@gmail.com",
+        },
+      ];
+      const subject = `Contact from ${name} (${email})`;
+      const body = message;
+      sendpulse.smtpSendMail(
+        (response) => {
+          console.log(response);
+          res.send(response);
+        },
+        from,
+        to,
+        subject,
+        body
+      );
+    } else {
+      console.error("Failed to authorize in SendPulse");
+      res.status(500).send("Failed to send email");
+    }
+  });
 });
 
 export { emailRouter as contactusEmail };
-
